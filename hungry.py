5c53ef75f7f34b7bd390dd0101e28a5f41f1f18a
kingsley <kingkatandawa@gmail.com>
2024-07-20 22:05:17 +0700

    Added Fries

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry?  ")
 if hungry == "yes" or hungry == "Yes":
     print("There is food in the fridge")
+    print("There is fries also")
 else:
     print("Do your work ")
     print("Or you can take a nap")
