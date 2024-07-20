6e5f6dbca8b1d8dbc1cab8eeb00bb82bd311c630
kingsley <kingkatandawa@gmail.com>
2024-07-20 15:14:21 +0700

    Revert "Added Ice cream"
    This reverts commit e00321ac7f36310232849434aeb5ea85a93bd385.

--- hungry.py
+++ hungry.py
@@ -1,7 +1,6 @@
 hungry = input("Are you hungry?  ")
 if hungry == "yes" or hungry == "Yes":
     print("There is food in the fridge")
-    print("Ice cream also")
 else:
     print("Do your work ")
     print("Or you can take a nap")
