e00321ac7f36310232849434aeb5ea85a93bd385
kingsley <kingkatandawa@gmail.com>
2024-07-20 15:10:39 +0700

    Added Ice cream

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry?  ")
 if hungry == "yes" or hungry == "Yes":
     print("There is food in the fridge")
+    print("Ice cream also")
 else:
     print("Do your work ")
     print("Or you can take a nap")
